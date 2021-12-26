b2d0fb12d7177ec09fdcefdcdae87619d1f9898e
Watermelon207 <88516062+Watermelon207@users.noreply.github.com>
2021-12-26 18:01:11 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var backgroundIMG
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+var score=0
+var goalIMG
+//const Body = Matter.Body;
+//const Render = Matter.Render;
+//const Constraint = Matter.Constraint;
+var engine, world
+var balls = []
+var ball;
+function preload()
+{
+backgroundIMG=loadImage("Stadium.jpg");
+}
+
+function setup() {
+createCanvas(1200,700);
+engine=Engine.create();
+world=engine.world;
+goal=new Goal(50,100,70,80);
+
+//ground= new Ground(600,380,,600);
+ground2 = new Ground(600,690,1200,20)
+ground3 = new Ground(10,100,20,1200)
+player=new Player(1150,550)
+ball = new Ball(player.body.position.x,player.body.position.y);
+}
+
+function draw() {
+console.log(player.body.position.x);
+background(backgroundIMG);
+Engine.update(engine);
+goal.display();
+
+ground2.display();
+ball.display();
+player.display();
+
+collisionWithBall();
+textSize(40);
+text("Score:"+score,1000,100)
+
+
+
+
+
+}
+
+function mousePressed()
+{
+//  console.log(mouse)
+   ball.shoot();
+   player.flag=true
+}
+function collisionWithBall() {
+  //for (var i = 0; i < balls.length; i++) {
+   // if (balls[index] !== undefined) {
+      var collision = Matter.SAT.collides(ball.body, goal.body);
+
+      if (collision.collided) {
+        score+=3
+         // balls.remove(i);
+        
+
+       // Matter.World.remove(world, ball.body);
+        //delete balls[index];
+      }
+    }
+  
